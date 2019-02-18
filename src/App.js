63c333291e7ee9cb683a982842b34204fb9f8c4f
Toro <toro@Torel.local>
2019-02-18 18:42:55 +0200

    askkasd

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import Header from "./components/Header";
+import Player from "./components/Player";
 
 class App extends Component {
+  state = {
+    players: [
+      {
+        name: "Michael",
+        id: 1
+      },
+      {
+        name: "Andreea",
+        id: 2
+      },
+      {
+        name: "John",
+        id: 3
+      },
+      {
+        name: "Gabi",
+        id: 4
+      }
+    ]
+  };
+
+  handleRemovePlayer = (id) => {
+    this.setState(function(prevState) {
+      return {
+        players: prevState.players.filter(function(p) {
+          return p.id !== id;
+        })
+      };
+    });
+  };
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="scorebord">
+        <Header title="Scorebord" totalPlayers={this.state.players.length} />
+
+        {this.state.players.map((player) => {
+          return (
+            <Player
+              name={player.name}
+              id={player.id}
+              key={player.id.toString()}
+              removePlayer={this.handleRemovePlayer}
+            />
+          );
+        })}
       </div>
     );
   }
